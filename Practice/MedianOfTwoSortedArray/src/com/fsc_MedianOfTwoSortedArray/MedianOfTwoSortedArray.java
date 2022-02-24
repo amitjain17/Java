82be4bf4ec9af073d8ad4e36875e352b9a82852a
Amit Jain <amitjainpwr@gmail.com>
2022-02-25 00:55:56 +0530

    Do changes

--- Practice/MedianOfTwoSortedArray/src/com/fsc_MedianOfTwoSortedArray/MedianOfTwoSortedArray.java
+++ Practice/MedianOfTwoSortedArray/src/com/fsc_MedianOfTwoSortedArray/MedianOfTwoSortedArray.java
@@ -6,6 +6,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collector;
 
+
+
 //Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 //
 //The overall run time complexity should be O(log (m+n)).
@@ -21,29 +23,26 @@ public class MedianOfTwoSortedArray {
 	public static void main(String args[]) {
 		
 		int[] num1 = {1,3};
-		int[] num2 = {2};
-		
+		int[] num2 = {2,3,4,5,6,7,9};
+		List<Integer> list = new ArrayList<>();
+
+
 		List<Integer> num3 = new ArrayList<>(Arrays.stream(num1).boxed().toList());
 		List<Integer> num4 = new ArrayList<>(Arrays.stream(num2).boxed().toList());
 		
 		
 		num3.forEach((e)-> num4.add(e));
-
 		num4.sort(null);
+		
+		int val = num4.size()/2;
 
 		if((num4.size())%2 == 0) {
-			int val = num4.size()/2;
-			System.out.println(num4.get(val));
+			double result = (double)(num4.get(val)+num4.get(val-1))/2;
+			System.out.println(result);
 		}else {
-			int val = num4.size()/2;
-			System.out.println(num4.get(val+1));
+			System.out.println(num4.get(val));
 		}
-		
-		//num3.sort(null);
-//		for(int i:num3) {
-//			System.out.println(i);
-//		}
-		
+			
 				
 	}
 
