c7420c962e75ef4ff5a2eae5f5d5917f1578dd21
Amit Jain <amitjainpwr@gmail.com>
2024-02-27 18:03:06 +0530

    controller updated

--- MicroserviceProject1/userservice/src/main/java/com/project/userservice/controller/UserController.java
+++ MicroserviceProject1/userservice/src/main/java/com/project/userservice/controller/UserController.java
@@ -2,13 +2,13 @@ package com.project.userservice.controller;
 
 import java.util.Date;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-	@RequestMapping("/healthCheck")
+	@GetMapping("/healthCheck")
 	public String healthCheck() {
         Date d1 = new Date(); 
         String result  = "Application is working at time: " + d1;
